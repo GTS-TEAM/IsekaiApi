aae4485c5a8bba141c48e1a60657cb066f3e40b7
Minh Nguyen <3langn@gmail.com>
2022-01-26 00:21:03 +0700

    upload multi files

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -3,9 +3,11 @@ import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
 import { Readable } from 'stream';
 @Injectable()
 export class CloudinaryService {
-  async uploadImage(
-    file: Express.Multer.File,
-  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+  async uploadImage(files: Array<Express.Multer.File>) {
+    return Promise.all(files.map((file) => this.uploadStream(file)));
+  }
+
+  async uploadStream(file: Express.Multer.File): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return new Promise((resolve, reject) => {
       const upload = v2.uploader.upload_stream((error, result) => {
         if (error) return reject(error);
