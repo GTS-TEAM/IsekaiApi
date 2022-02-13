c5c8b8e02a7ab2cdbb8f8e331dc881ff878bfa27
Minh Nguyen <3langn@gmail.com>
2022-02-14 02:44:29 +0700

    fix upload service

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CloudinaryProvider } from './cloudinary.provider';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 
 @Module({
   controllers: [UploadController],
-  providers: [UploadService],
-  exports: [UploadService],
+  providers: [CloudinaryProvider, UploadService],
+  exports: [CloudinaryProvider, UploadService],
 })
 export class UploadModule {}
